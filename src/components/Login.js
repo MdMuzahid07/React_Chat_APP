fcd777449b9262c83279fd1949f9359e0325111e
mdmuzahid07 <mdmuzahid7396@gmail.com>
2022-07-11 00:24:25 +0600

    working on it

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div id='login-page'>
+            <div id="login-card">
+                <h2>Welcome to cyber_chat</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
